b67edebf1da6a62c08b085b917d6aabbd22eee1a
saakethvishnumolak <saavish@iu.edu>
2026-01-21 20:49:26 +0000

    Complete Exercise 1

--- apputil.py
+++ apputil.py
@@ -1,3 +1,16 @@
+def palindrome(word):
+    
+    word = word.lower()
 
+    cleanWord = ''
+
+    for letter in word:
+        if letter.isalpha():
+            cleanWord += letter
+
+
+    return cleanWord == cleanWord[::-1]
+
+    
 
 # add code below ...
